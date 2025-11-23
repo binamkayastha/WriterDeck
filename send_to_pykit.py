db410b632ac2372a85a49369dac29c42b156ea6b
keerthi-manur <112362224+keerthi-manur@users.noreply.github.com>
2025-11-23 17:31:04 -0500

    Add functionality to send data to PyKit display

---
+++ send_to_pykit.py
@@ -0,0 +1,23 @@
+import serial
+import serial.tools.list_ports
+ports = serial.tools.list_ports.comports()
+pykit_port = None
+
+for port in ports:
+    if 'USB' in port.description or 'CircuitPython' in port.description:
+        pykit_port = port.device
+        break
+
+if not pykit_port:
+    print("PyKit not found!")
+    exit()
+
+print(f"Connected to PyKit on {pykit_port}")
+ser = serial.Serial(pykit_port, 115200, timeout=1)
+
+print("Display connected.")
+
+while True:
+    user_input = input("Type here: ")
+    ser.write((user_input + '\n').encode())
+    print(f"Sent to display: {user_input}")
