a69a76ca64433460f847ae34da24ffc71a8a41af
binamkayastha <binamkayastha@gmail.com>
2025-11-23 21:30:44 -0500

    Update send_to_pykit with code running on raspberry pi and update README

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 **Welcome to the Writer Deck Github Repo!**
 
+This project was built for the Sundai hackathon. Check out the project demo on our sundai card:
+
+https://www.sundai.club/projects/5b6a7d8b-5b31-4045-8678-3e21955fad8b
+
 This is the repo for our offline, distraction-free typing screen. Work on your novels, poems, assignments, or fanfiction on the Writer Deck without falling victim to doomscrolling!
 
 Equipment used: Curiosity CircuitPython, Raspberry Pi 3
 
+This works by:
 
+- Keyboard input which is connected to the raspberry pi
+- The Raspberry Pi is connected to the Arudino Ruler (not yet released device that we got to play with for the sundai hack) and streams the keyboard inputs via serial port through send_to_pykit.py. This is python program runs in a terminal which the keyboard is focused on.
+- The ruler recieves the data via sysin then updates the screen via sysout
--- send_to_pykit.py
+++ send_to_pykit.py
@@ -1,6 +1,8 @@
+import sys, tty, termios
+
 import serial
 import serial.tools.list_ports
-ports = serial.tools.list_ports.comports()
+ports= serial.tools.list_ports.comports()
 pykit_port = None
 
 for port in ports:
@@ -10,14 +12,21 @@ for port in ports:
 
 if not pykit_port:
     print("PyKit not found!")
-    exit()
+    exit ()
 
-print(f"Connected to PyKit on {pykit_port}")
+print(f"Connected to Pykit on {pykit_port}")
 ser = serial.Serial(pykit_port, 115200, timeout=1)
 
-print("Display connected.")
+fd = sys.stdin.fileno()
+old_settings = termios.tcgetattr(fd)
 
-while True:
-    user_input = input("Type here: ")
-    ser.write((user_input + '\n').encode())
-    print(f"Sent to display: {user_input}")
+try:
+    tty.setraw(sys.stdin.fileno())
+    while True:
+        ch = sys.stdin.read (1) # reads instantly
+        if ch == '\x1b': # ESC key
+            break
+        print("Pressed:" , repr(ch))
+        ser.write(ch.encode ())
+finally:
+    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
