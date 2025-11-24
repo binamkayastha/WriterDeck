6ada6e1d01f4e67a4aeb4f6f1266d356bcac4dfb
binamkayastha <binamkayastha@gmail.com>
2025-11-23 19:16:40 -0500

    Add send_to_pykit_realtime.py

---
+++ send_to_pykit_realtime.py
@@ -0,0 +1,29 @@
+from pynput import keyboard
+import serial
+import serial.tools.list_ports
+ports = serial.tools.list_ports.comports()
+pykit_port = None
+
+for port in ports:
+    if 'USB' in port.description or 'CircuitPython' in port.description:
+        pykit_port = port.device
+        break
+
+if not pykit_port:
+    print("PyKit not found!")
+    exit()
+
+print(f"Connected to PyKit on {pykit_port}")
+ser = serial.Serial(pykit_port, 115200, timeout=1)
+
+print("Display connected.")
+
+with keyboard.Events() as events:
+    while True:
+        # Block for as much as possible
+        event = events.get(1e6)
+
+        # ser.write((user_input + '\n').encode())
+        ser.write((str(event.key)).encode())
+
+
